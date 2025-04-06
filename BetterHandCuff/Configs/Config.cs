b477c30f0674d9e05fec91bd4e4e6c4fca97faf5
Ezua <choinka441.gry@gmail.com>
2025-04-06 21:08:00 +0200

    Improved config. Removed spaces in file name.

--- BetterHandCuff/Configs/Config.cs
+++ BetterHandCuff/Configs/Config.cs
@@ -12,15 +12,15 @@ namespace BetterHandCuff
         [Description("Debug doesn't work yet.")]
         public bool Debug { get; set; }
 
-        public int Time { get; set; } = 3;
+        public int Time { get; set; } = 4;
 
         public float Range { get; set; } = 10;
         
         public int Distance { get; set; } = 2;
 
-        public int MaxHandCuff { get; set; } = 10;
+        public int MaxHandCuff { get; set; } = 8;
         [Description("This class will get a handcuff on start of the game")]
-        public HashSet<RoleTypeId> HandCuffOnStart { get; set; } = new() { RoleTypeId.ChaosMarauder, RoleTypeId.ChaosConscript };
+        public HashSet<RoleTypeId> HandCuffOnStart { get; set; } = new() { RoleTypeId.ChaosMarauder, RoleTypeId.ChaosConscript, RoleTypeId.ChaosRifleman, RoleTypeId.ChaosRepressor, RoleTypeId.NtfCaptain,RoleTypeId.NtfPrivate,RoleTypeId.NtfSergeant,RoleTypeId.NtfSpecialist, RoleTypeId.FacilityGuard};
         [Description("How many handcuff player that is in HandCuffOnStart will get on spawn as that role.")]
         public int HowManyHandCuffAdd { get; set; } = 2;
     }
