742fb81cce6aa4ee0cc14dee2d2ba16ae1e86b0c
Ezua <choinka441.gry@gmail.com>
2025-06-18 14:43:13 +0200

    change

--- BetterHandCuff/Program.cs
+++ BetterHandCuff/Program.cs
@@ -14,7 +14,7 @@ namespace BetterHandCuff
         public override string Author => "Ezua";
         public override string Name => "BetterHandCuff";
         public override string Prefix => "BetterHandCuff";
-        public override Version Version { get; } = new(1, 2, 0);
+        public override Version Version { get; } = new(1, 2, 2);
         public override Version RequiredExiledVersion { get; } = new(9, 6, 1, 0);
         public override PluginPriority Priority => PluginPriority.Higher;
 
