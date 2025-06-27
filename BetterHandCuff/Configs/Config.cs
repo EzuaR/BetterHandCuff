9f70ca2bde5d40fbb0ba96658d71d8a159f1df43
Ezua <choinka441.gry@gmail.com>
2025-06-27 13:00:13 +0200

    small changes

--- BetterHandCuff/Configs/Config.cs
+++ BetterHandCuff/Configs/Config.cs
@@ -10,11 +10,15 @@ namespace BetterHandCuff.Configs
         public bool IsEnabled { get; set; } = true;
         [Description("Debug doesn't work yet.")]
         public bool Debug { get; set; }
+        [Description("Time of hints.")]
         public int Time { get; set; } = 4;
+        [Description("Range of cuffing commands.")]
         public float Range { get; set; } = 10;
-        public int Distance { get; set; } = 2;
+        [Description("Range of loot command.")]
+        public int Distance { get; set; } = 5;
+        [Description("max amount of handcuffs")]
         public int MaxHandCuff { get; set; } = 18;
-        [Description("This class will get a handcuff on start of the game")]
+        [Description("This class will get a handcuff on start of the game of if InfinityHandCuffs are enab")]
         public HashSet<RoleTypeId> HandCuffOnStart { get; set; } = new() { RoleTypeId.ChaosMarauder, RoleTypeId.ChaosConscript, RoleTypeId.ChaosRifleman, RoleTypeId.ChaosRepressor, RoleTypeId.NtfCaptain, RoleTypeId.NtfPrivate, RoleTypeId.NtfSergeant, RoleTypeId.NtfSpecialist, RoleTypeId.FacilityGuard };
         [Description("How many handcuff player that is in HandCuffOnStart will get on spawn as that role.")]
         public int HowManyHandCuffAdd { get; set; } = 6;
@@ -24,5 +28,6 @@ namespace BetterHandCuff.Configs
         public bool HandCuffPlayerFromTheSameTeam { get; set; } = true;
         [Description("Allows to have unlimited amount of handcuffs.")]
         public bool InfinityHandCuffs { get; set; } = false;
+        
     }
 }
